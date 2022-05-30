a91371caf37342a7b30fbbc93fce55ced9b0fc1e
Ivan Kornev <ivan.kornev@evocargo.com>
2022-05-31 00:06:32 +0300

    Replace docuementation into documentation folder

--- arduino_code/Version_03_12_17/sketch_dec03b/sketch_dec03b.ino
+++ arduino_code/Version_03_12_17/sketch_dec03b/sketch_dec03b.ino
@@ -1,8 +1,12 @@
 const int relays [17] = {13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 14, 15, 16, 17, 18};
-//const int signalFromMultimeter [28] = {22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 21};
-const int signalFromMultimeter [28] = {45, 46, 39, 40, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 41, 42, 43, 44, 47, 48, 21};
+const int signalFromMultimeter [28] = {21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48};
+
+volatile bool start = false;
+volatile bool valueWasReceived = false;
+
 volatile int value = 0;
-volatile int numberOfRelay = 0;
+
+volatile bool state = false;
 
 void setup() {
   Serial.begin(9600);
@@ -17,32 +21,41 @@ void setup() {
   for(int i = 0; i < 28; i++){
     pinMode(signalFromMultimeter[i], INPUT);
   }
+  
+  // Прервание по нажатию кнопки "ПУСК", INT.2 PIN 21
+  // attachInterrupt(2, startProgramm, RISING);
 }
 
 
 void loop() {
-  char incomingChar;
+  char IncomingChar;
+  
   if(Serial.available() > 0){
-    incomingChar = Serial.read();
-    switch(incomingChar){
-      case'n':
-        digitalWrite(relays[numberOfRelay], HIGH);
+    IncomingChar = Serial.read();
+    switch(IncomingChar){
+      case '1': 
+        for (int j = 0; j < 6; j++) for(int i = 0; i < 4; i++) Serial.print(random(0, 2));
         break;
-      case'r':
-        attachInterrupt(3, readValues, RISING);
+      case '0':
         break;
-      case'e':
-        numberOfRelay = 0;
-        break;      
     }
   }
+  delay(300);
 }
 
-void readValues(){
-  for (int i = 0; i < 25; i++){
+void readOneValue() {
+  detachInterrupt(3);
+  for (int i = 1; i < 5; i++){
     value = digitalRead(signalFromMultimeter[i]);
     Serial.print(value);
   }
-  digitalWrite(relays[numberOfRelay++], LOW);
-  detachInterrupt(3);
+  Serial.println();
+  attachInterrupt(2, startProgramm, RISING);
+}
+
+// сюда будут переходить прерывания от "ПУСК" и от "USART"
+// дают "добро" на запуск программы
+void startProgramm(){ 
+  detachInterrupt(2);
+  attachInterrupt(3, readOneValue, RISING);
 }
