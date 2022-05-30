3c30a3bdd8221bf463434bf431169691e467f935
Ivan Kornev <ivan.kornev@evocargo.com>
2022-05-31 00:18:12 +0300

    Update: arduino code autoformatted

--- arduino_code/Example/Example.ino
+++ arduino_code/Example/Example.ino
@@ -1,5 +1,8 @@
-const int relays [17] = {13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 14, 15, 16, 17, 18};
-const int signalFromMultimeter [28] = {21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48};
+const int relays[17] = {13, 12, 11, 10, 9,  8,  7,  6, 5,
+                        4,  3,  2,  14, 15, 16, 17, 18};
+const int signalFromMultimeter[28] = {21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+                                      31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+                                      41, 42, 43, 44, 45, 46, 47, 48};
 
 volatile bool start = false;
 volatile bool valueWasReceived = false;
@@ -8,42 +11,43 @@ volatile int value = 0;
 
 void setup() {
   Serial.begin(9600);
-  
+
   // Порты для управления реле настраиваютяс на выход
   // После инициализации на выходе 0В
-  for (int i = 0; i < 17; i++){
+  for (int i = 0; i < 17; i++) {
     pinMode(relays[i], OUTPUT);
     digitalWrite(relays[i], LOW);
   }
   // Порты для принятия сигнала настраиваются на вход
-  for(int i = 0; i < 28; i++){
+  for (int i = 0; i < 28; i++) {
     pinMode(signalFromMultimeter[i], INPUT);
   }
-  
+
   // Прервание по нажатию кнопки "ПУСК", INT.2 PIN 21
   attachInterrupt(2, startProgramm, CHANGE);
 }
 
-
 void loop() {
-  if (start){
+  if (start) {
     // readWriteAllValues();  закомменчено только для дебага
 
     //######## Код для дебага #######
     delay(1000);
     int count = 0;
     attachInterrupt(3, readOneValue, FALLING);
-    while (valueWasReceived == false){
+    while (valueWasReceived == false) {
       count = 1;
-      }
+    }
     valueWasReceived = false;
     Serial.print("; counter = ");
     Serial.println(count);
     //##### конец кода для дебага ####
-    
-    start = false;  // Программа выполняется всего 1 раз
-    
-    attachInterrupt(2, startProgramm, CHANGE); // Снова разрешили прерывание по нажатию кнпоки "ПУСК"
+
+    start = false; // Программа выполняется всего 1 раз
+
+    attachInterrupt(
+        2, startProgramm,
+        CHANGE); // Снова разрешили прерывание по нажатию кнпоки "ПУСК"
   }
   delay(500);
 }
@@ -54,14 +58,15 @@ void readWriteAllValues(){
     digitalWrite(relays[i], HIGH);
     delay(5);
     attachInterrupt(3, readOneValue, RISING);
-    while (!valueWasReceived);  // Опасный момент, можем зависнуть здесь навсегда
+    while (!valueWasReceived);  // Опасный момент, можем зависнуть здесь
+навсегда
   }
 }
 */
 
 void readOneValue() {
   detachInterrupt(3);
-  for (int i = 1; i < 5; i++){
+  for (int i = 1; i < 5; i++) {
     value = digitalRead(signalFromMultimeter[i]);
     Serial.print(value);
   }
@@ -78,7 +83,9 @@ ISR (USART0_RX_vect){
 
 // сюда будут переходить прерывания от "ПУСК" и от "USART"
 // дают "добро" на запуск программы
-void startProgramm(){
-  if (!start) start = true;
-  detachInterrupt(2); // Запретили на всякий случай прервание по нажатию кнопки "ПУСК"
+void startProgramm() {
+  if (!start)
+    start = true;
+  detachInterrupt(
+      2); // Запретили на всякий случай прервание по нажатию кнопки "ПУСК"
 }
--- arduino_code/Version_03_12_17/sketch_dec03b/sketch_dec03b.ino
+++ arduino_code/Version_03_12_17/sketch_dec03b/sketch_dec03b.ino
@@ -1,5 +1,8 @@
-const int relays [17] = {13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 14, 15, 16, 17, 18};
-const int signalFromMultimeter [28] = {21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48};
+const int relays[17] = {13, 12, 11, 10, 9,  8,  7,  6, 5,
+                        4,  3,  2,  14, 15, 16, 17, 18};
+const int signalFromMultimeter[28] = {21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+                                      31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+                                      41, 42, 43, 44, 45, 46, 47, 48};
 
 volatile bool start = false;
 volatile bool valueWasReceived = false;
@@ -13,12 +16,12 @@ void setup() {
 
   // Порты для управления реле настраиваютяс на выход
   // После инициализации на выходе 0В
-  for (int i = 0; i < 17; i++){
+  for (int i = 0; i < 17; i++) {
     pinMode(relays[i], OUTPUT);
     digitalWrite(relays[i], LOW);
   }
   // Порты для принятия сигнала настраиваются на вход
-  for(int i = 0; i < 28; i++){
+  for (int i = 0; i < 28; i++) {
     pinMode(signalFromMultimeter[i], INPUT);
   }
 
@@ -26,18 +29,19 @@ void setup() {
   // attachInterrupt(2, startProgramm, RISING);
 }
 
-
 void loop() {
   char IncomingChar;
 
-  if(Serial.available() > 0){
+  if (Serial.available() > 0) {
     IncomingChar = Serial.read();
-    switch(IncomingChar){
-      case '1':
-        for (int j = 0; j < 6; j++) for(int i = 0; i < 4; i++) Serial.print(random(0, 2));
-        break;
-      case '0':
-        break;
+    switch (IncomingChar) {
+    case '1':
+      for (int j = 0; j < 6; j++)
+        for (int i = 0; i < 4; i++)
+          Serial.print(random(0, 2));
+      break;
+    case '0':
+      break;
     }
   }
   delay(300);
@@ -45,7 +49,7 @@ void loop() {
 
 void readOneValue() {
   detachInterrupt(3);
-  for (int i = 1; i < 5; i++){
+  for (int i = 1; i < 5; i++) {
     value = digitalRead(signalFromMultimeter[i]);
     Serial.print(value);
   }
@@ -55,7 +59,7 @@ void readOneValue() {
 
 // сюда будут переходить прерывания от "ПУСК" и от "USART"
 // дают "добро" на запуск программы
-void startProgramm(){
+void startProgramm() {
   detachInterrupt(2);
   attachInterrupt(3, readOneValue, RISING);
 }
